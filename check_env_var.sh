8d688792b19ca402c65247735084978c78148d55
kerteszakos <130358476+kerteszakos@users.noreply.github.com>
2023-04-15 15:30:53 +0200

    Update check_env_var.sh

--- check_env_var.sh
+++ check_env_var.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Check if environment variable is set, if not throw error
 if [[ -z $ISITCURRENTLYFISHY ]]; then
-  >&2 echo "Error: ISITCURRENTLYFISHY environment variable is not set"
+  2>&1 echo "Error: ISITCURRENTLYFISHY environment variable is not set"
+#  echo "Error: ISITCURRENTLYFISHY environment variable is not set" >> /dev/stderr
 fi
